10f9c6ad79fa5584fc3b6831774cea88e5ddd164
viviboda <vivien.boda@ibello.hu>
2020-12-21 09:06:02 +0100

    :ambulance: getProductName is changed to productNames list

--- ibello-hu/src/main/java/hu/ibello/demo/steps/ChoosingProductSteps.java
+++ ibello-hu/src/main/java/hu/ibello/demo/steps/ChoosingProductSteps.java
@@ -5,10 +5,15 @@ import hu.ibello.demo.model.ProductToOrder;
 import hu.ibello.demo.pages.OrderPage;
 import hu.ibello.steps.StepLibrary;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Name("Choosing product to order")
 public class ChoosingProductSteps extends StepLibrary {
 
     private OrderPage orderPage;
+    private final List<String> productNames = new ArrayList<>(Arrays.asList("evaluation", "serves", "analyzer", "creator", "team 5", "team 10"));
 
     public void i_use_$_test_data_to_select_products(ProductToOrder data) {
         if (data != null) {
@@ -33,4 +38,39 @@ public class ChoosingProductSteps extends StepLibrary {
         }
     }
 
+    public void i_see_that_server_is_selected() {
+        check_if_order_with_$_index_is_selected(1);
+    }
+
+    public void i_see_that_analyzer_is_selected() {
+        check_if_order_with_$_index_is_selected(2);
+    }
+
+    public void i_see_that_creator_is_selected() {
+        check_if_order_with_$_index_is_selected(3);
+    }
+
+    public void i_see_that_team_10_is_selected() {
+        check_if_order_with_$_index_is_selected(5);
+    }
+
+    private void check_if_order_with_$_index_is_selected(int productIndex) {
+        String product;
+        if (productNames.size()<=productIndex) {
+            product = "";
+        } else {
+            product = productNames.get(productIndex);
+        }
+        for (int index = 0; index < 4; index++) {
+            int currentNum = Integer.parseInt(orderPage.getNumberOfProduct(index));
+            if (index == productIndex) {
+                if (currentNum != 1) {
+                    throw new AssertionError(String.format("The %s is not selected!", product));
+                }
+            }
+            if (currentNum > 0 && index != productIndex) {
+                throw new AssertionError("There is unexpected product selection!");
+            }
+        }
+    }
 }
